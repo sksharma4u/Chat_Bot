bfca4b386088373cae8c52e23a2901bae1de11a8
sharmaji2612 <mahendrapal2612@gmail.com>
2023-05-13 01:02:25 +0530

    Optimize folder hierarchy

--- Bot_Application/server/index.js
+++ Bot_Application/server/index.js
@@ -1,43 +1,22 @@
 const express = require('express');
+const path = require('path')
+    //Require my Env file
+const dotenv = require('dotenv');
+const envPath = path.resolve(__dirname, '../.env');
+
+dotenv.config({ path: envPath });
+
 const bodyParser = require('body-parser');
 const app = express();
-const sendMessage = require('../whatsApp/sendMessage');
-const message = require('./message');
-const information = require('./dataGathering')
+const replyService = require('./replyContollers/replyService');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-    console.log("Server is working")
-    app.get("/", (req, res) => {
-        res.send("hii its working");
-    })
-})
 
+const PORT = process.env.PORT;
 
-
-var count = 0;
-app.post('/reply', async(req, res) => {
-    var recievedMessage = req.body.Body;
-    recievedMessage = recievedMessage.toLowerCase();
-    const senderId = req.body.From;
-
-    console.log(recievedMessage);
-
-    if (recievedMessage == "reset" || recievedMessage == "no") {
-        information.clearResponse();
-        count = 0;
-        await sendMessage.messageSend(message.message, senderId);
-    } else if (recievedMessage == "create") {
-        count++;
-        await sendMessage.messageSend("What is you Name: ", senderId);
-    } else if (recievedMessage == "yes") {
-        await sendMessage.messageSend("Thank you for providing the necessary information! We have received your responses and will now proceed to prepare your resume. Please wait for a moment while we generate your personalized resume.", senderId)
-    } else if (count == 1) {
-        information.userResponse(senderId, recievedMessage);
-    } else if (recievedMessage == "hii" || recievedMessage == "hello" || recievedMessage == "hey") {
-        await sendMessage.messageSend(message.message, senderId);
-    } else {
-        await sendMessage.messageSend("Invalid Response  , only create and get Command works", senderId)
-    }
+app.listen(PORT, () => {
+    console.log(`Server is working at ${PORT}`)
+    replyService.replyHandler(app);
 })
--- Bot_Application/server/message.js
+++ Bot_Application/server/message.js
@@ -1,21 +1,38 @@
+const emoji = '\u{1F600}';
+const smile = `\u{1F60A}`;
+const heart = `\u{2764}\u{FE0F}`;
+const fire = `\u{1F525}`;
+const heartInEye = `\u{1F60D}`;
+const thumbsUp = `\u{1F44D}`;
+const clap = `\u{1F44F}`;
+const oneNumber = `\u{0031}\u{FE0F}\u{20E3}`
+const twoNumber = `\u{0032}\u{FE0F}\u{20E3}`
+const threeNumber = `\u{0033}\u{FE0F}\u{20E3}`
+
+
 const message = `
-Welcome to our Resume Chat Bot!
+Welcome to our Resume Chat Bot!${clap}
 
-I 'm here to assist you in creating and 
-retrieving resumes quickly and efficiently.
+I'm here to assist you in creating and retrieving resumes quickly and efficiently.${String.fromCodePoint(0x1F4D1)}
         
-With just a few simple commands, you'll be 
-able to craft a professional resume or retrieve 
-an existing one.
+With just a few simple commands, you'll be able to craft a professional resume or retrieve an existing one.
         
-To get started, here are the available commands:
+To get started, here are the available commands:  ${String.fromCodePoint(0x1F4DD)}
 
-1: create: This command allows you to create a 
-new resume.
+${oneNumber} Create:This command allows you to create a new resume.
 
-2: get: This command enables you to retrieve a 
-previously created resume.`
+${twoNumber} Get: This command enables you to retrieve a previously created resume.
+
+${threeNumber} Reset : This command allow you to reset/start the whole bot`
+
+
+const ThankYouMessage = `Thank you for providing the necessary information! ${smile}${smile}
+We have received your responses ${String.fromCodePoint(0x2714)} and will now proceed to prepare your resume ${String.fromCodePoint(0x1F4BB)}. 
+Please wait for a moment ${String.fromCodePoint(0x23F3)} while we generate your personalized resume ${String.fromCodePoint(0x1F4D1)}.`;
+
+
 
 module.exports = {
-    message
+    message,
+    ThankYouMessage
 }
